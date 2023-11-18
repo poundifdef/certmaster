84938512f22ec052c7fc0421106b9ead5a3e7fd7
jgoel <jay@hotswap.app>
2023-11-18 16:16:01 -0500

    Unset aws session token in lambda

--- lambda.go
+++ lambda.go
@@ -3,12 +3,16 @@ package main
 import (
 	"certmaster/models"
 	"fmt"
+	"os"
 )
 
 func HandleLambdaEvent(event *models.CertRequest) (*models.CertResponse, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
+
+	// Delete this token and use whatever token is passed in
+	os.Unsetenv("AWS_SESSION_TOKEN")
 
 	err := createCert(event)
 
